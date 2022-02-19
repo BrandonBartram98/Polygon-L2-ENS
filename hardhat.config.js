0e8e5b966080af520206567d4d7425cea58cbc34
Brandon Bartram <brandon@immersivevr.co.uk>
2022-02-19 22:23:56 +0000

    Added back hardhat config, made env copy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,8 +21,13 @@ module.exports = {
   solidity: "0.8.10",
   networks: {
 		mumbai: {
-      url: YOUR_ALCHEMY_MUMBAI_URL,
-      accounts: [PRIVATE_KEY],
-		}
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+		},
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   }
 };
