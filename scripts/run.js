1512fd7bd78f9e1d497a086e477116beab026962
Brandon Bartram <brandon@immersivevr.co.uk>
2022-02-19 16:08:55 +0000

    SVG generation

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,21 @@
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
-    await domainContract.deployed();
+      // We pass in "ninja" to the constructor when deploying
+      const domainContract = await domainContractFactory.deploy("hive");
+      await domainContract.deployed();
+  
     console.log("Contract deployed to:", domainContract.address);
-  };
+      
+      // We're passing in a second variable - value. This is the moneyyyyyyyyyy
+      let txn = await domainContract.register("honey",  {value: hre.ethers.utils.parseEther('0.1')});
+      await txn.wait();
+  
+    const address = await domainContract.getAddress("honey");
+    console.log("Owner of domain honey:", address);
+  
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+  }
   
   const runMain = async () => {
     try {
